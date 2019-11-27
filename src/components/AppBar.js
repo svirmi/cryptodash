d1c33cc4da8976a0843d2cc20a56645e680ef8f2
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-11-27 08:22:33 +0200

    grid splitted to columns

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -3,8 +3,13 @@ import styled from "styled-components";
 
 const Bar = styled.div`
     display:grid;
+    grid-template-columns: 180px 100px 100px;
 `;
 
 export default function () {
-    return <Bar> AppBar </Bar>
+    return  <Bar>
+                <div>Item 1</div>
+                <div>Item 2</div>
+                <div>Item 3</div>
+            </Bar>
 }
