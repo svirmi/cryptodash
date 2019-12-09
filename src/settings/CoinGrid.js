af616cf2c8dd7606b838874d3e3e1bb14e40b098
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-12-09 08:26:00 +0200

    handling favorites, initial setup

--- src/settings/CoinGrid.js
+++ src/settings/CoinGrid.js
@@ -10,15 +10,15 @@ export const CoinGridStyled = styled.div`
     margin: 30px auto;
 `;
 
-function getCoinsToDisplay(coinList, topSection) {
-    return Object.keys(coinList).slice(0,topSection ? 10 : 100);
+function getCoinsToDisplay(coinList, topSection, favorites) {
+    return topSection ? favorites : Object.keys(coinList).slice(0,topSection ? 10 : 100);
 }
 
 export default function ({topSection}) {
     return <AppContext.Consumer>
-        {({coinList}) =>
+        {({coinList, favorites}) =>
             <CoinGridStyled>
-                {getCoinsToDisplay(coinList, topSection).map(coinKey =>
+                {getCoinsToDisplay(coinList, topSection, favorites).map(coinKey =>
                     <CoinTile key={coinKey} coinKey={coinKey} topSection={topSection}/>
                 )}
             </CoinGridStyled>
