ddbd3da6baf91dc27505576ee1d0d43789ef9e39
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-12-11 09:24:34 +0200

    styling Search

--- src/settings/Search.js
+++ src/settings/Search.js
@@ -1,7 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import {backgroundColor2, fontSize2} from "../shared/Styles";
 
+const SearchGrid = styled.div`
+    display: grid;
+    grid-template-columns: 200px 1fr;
+`;
+
+const SearchInput = styled.input`
+    ${backgroundColor2}
+    ${fontSize2}
+    border: 1px solid;
+    height: 2.5rem;
+    color: #1163c9;
+    place-self: center left;
+`;
 
 export default function () {
-    return <div>Search</div>
+    return (
+        <SearchGrid>
+           <h2>Search all coins</h2>
+            <SearchInput/>
+        </SearchGrid>
+    )
 }
