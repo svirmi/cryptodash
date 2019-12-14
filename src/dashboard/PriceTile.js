2f79d2bdf0f16839fdbf6da92167b0c05c7d1b05
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-12-14 14:13:46 +0200

    PriceTile, work in progress still

--- src/dashboard/PriceTile.js
+++ src/dashboard/PriceTile.js
@@ -2,10 +2,33 @@ import React from "react";
 import styled, {css} from "styled-components";
 import {SelectableTile} from "../shared/Tile";
 import {CURRENCY} from "../context/AppProvider";
+import {fontSize3} from "../shared/Styles";
+import {CoinHeaderGridStyled} from "../settings/CoinHeaderGrid";
+
+const PriceTileStyled = styled(SelectableTile)`
+    ${props => props.compact && css`
+        ${fontSize3}
+    `}
+`;
+
+function PriceTile({sym, data}) {
+    return (
+        <PriceTileStyled>
+            <CoinHeaderGridStyled>
+                <div>{sym}</div>
+                <div>{data.CHANGEPCT24HOUR}</div> // change percents 24 hrs
+            </CoinHeaderGridStyled>
+        </PriceTileStyled>
+    );
+}
 
 export default function ({price, index}) {
     let sym = Object.keys(price)[0];
     let data = price[sym][CURRENCY];
 
-    return <SelectableTile>{sym} {data.PRICE}</SelectableTile>
+    return (
+        <PriceTile sym={sym} data={data}>
+            {sym} {data.PRICE}
+        </PriceTile>
+    )
 }
--- src/shared/Styles.js
+++ src/shared/Styles.js
@@ -24,6 +24,6 @@ export const redBoxShadow = `box-shadow: 0px 0px 2px 2px #e41111;`;
 export const fontSizeBig = 'font-size: 2em';
 export const fontSize1 = 'font-size: 1.5em;';
 export const fontSize2 = 'font-size: 1.0em;';
-export const fontSize3 = 'font-size: .75em';
+export const fontSize3 = 'font-size: .75em;';
 
 export const textAlignCenter = 'text-align: center;';
