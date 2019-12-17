ccb9e592303cda172a524070083ff1e2714bd597
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-12-17 09:06:10 +0200

    styling CoinImage component

--- src/dashboard/CoinSpotlight.js
+++ src/dashboard/CoinSpotlight.js
@@ -1,14 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 import {Tile} from "../shared/Tile";
 import {AppContext} from "../context/AppProvider";
 import CoinImage from "../shared/CoinImage";
+
+const SpotLightName = styled.h2`
+    text-align: center;
+`;
+
 
 export default function () {
     return (
         <AppContext.Consumer>
             {({currentFavorite, coinList}) =>
                 <Tile>
-                    <h2>{coinList[currentFavorite].CoinName}</h2>
+                    <SpotLightName>{coinList[currentFavorite].CoinName}</SpotLightName>
+                    <CoinImage coin={coinList[currentFavorite]}/>
                 </Tile>
             }
         </AppContext.Consumer>
--- src/shared/CoinImage.js
+++ src/shared/CoinImage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+
+const CoinImage = styled.img`
+    height: 50px;
+`;
 
 export default function ({coin, style}) {
-    return <img
+    return <CoinImage
         alt={coin.CoinSymbol}
-        style={style || {height: '50px'}}
         src={`https://cryptocompare.com${
             coin.ImageUrl
         }`}
