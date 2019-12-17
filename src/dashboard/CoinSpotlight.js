b1bafe7144bf94d689a8e063c37c6f1287879b64
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-12-17 08:41:22 +0200

    Spotlight area setup

---
+++ src/dashboard/CoinSpotlight.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Tile} from "../shared/Tile";
+import {AppContext} from "../context/AppProvider";
+import CoinImage from "../shared/CoinImage";
+
+export default function () {
+    return <Tile>Spotlight area</Tile>
+}
--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Page from "../shared/Page";
 import PriceGrid from "./PriceGrid";
+import CoinSpotlight from "./CoinSpotlight";
 
 export default function () {
     return <Page name="dashboard">
         <PriceGrid/>
+        <CoinSpotlight/>
     </Page>
 }
