180778471a9df351d279f6338d5c476a39a7973a
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-12-21 12:13:19 +0200

    dark Highcharts theme

--- src/dashboard/PriceChart.js
+++ src/dashboard/PriceChart.js
@@ -4,7 +4,9 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import {AppContext} from "../context/AppProvider";
 import {Tile} from "../shared/Tile";
+import HighChartsTheme from "./HighChartsTheme";
 
+Highcharts.setOptions(HighChartsTheme);
 
 export default function () {
     return (
