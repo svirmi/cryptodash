5a2775262aea7a510305925ad966011669948151
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-12-21 17:26:46 +0200

    google fonts as a direct import

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,15 +8,5 @@ module.exports = {
     plugins: [
         `gatsby-plugin-styled-components`,
         `gatsby-plugin-lodash`,
-        {
-            resolve: `gatsby-plugin-prefetch-google-fonts`,
-            options: {
-                fonts: [
-                    {
-                        family: `Do Hyeon`,
-                    },
-                ],
-            },
-        }
     ],
 }
